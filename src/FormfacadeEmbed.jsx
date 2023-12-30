886b40377e184115b7f22b71bde3392f035cfabe
HariKarthyk <harikarthyk1407@gmail.com>
2023-12-30 17:23:43 +0530

    Refactor FormfacadeEmbed component and add React.memo optimization

--- src/FormfacadeEmbed.jsx
+++ src/FormfacadeEmbed.jsx
@@ -3,18 +3,16 @@ import React from 'react';
 const onSubmitFormHandlerDefault = () => {
     alert('Form submitted');
 }
+
 const FormfacadeEmbed = ({
     prefillFormFn = () => { return {}; },
     formFacadeEmbedURL,
     onSubmitFormHandler = onSubmitFormHandlerDefault
 }) => {
-
-
     React.useEffect(() => {
 
         const script = document.createElement('script');
 
-        // append prefill=prefillForm this at last, if already ? exist them append &prefill=prefillForm else append ?prefill=prefillForm
         formFacadeEmbedURL = formFacadeEmbedURL + (formFacadeEmbedURL.indexOf('?') > -1 ? '&' : '?') + 'prefill=ffPrefillForm';
 
         script.src = formFacadeEmbedURL;
@@ -22,22 +20,20 @@ const FormfacadeEmbed = ({
         script.defer = true;
         document.body.appendChild(script);
 
-        // @ts-ignore
         window.facadeListener = {
             onChange: function (arg) {
                 if (arg === 'submit') {
-                    // @ts-ignore
                     const submitSeq = window.formFacade?.result && window.formFacade?.result?.submitSeq;
 
                     if (submitSeq) {
-                        // @ts-ignore
-                        let payload = {
+                        onSubmitFormHandler({
                             type: 'FORM_SUBMITTED_SUCCESS',
-                            // @ts-ignore
-                            submitSeq: window.formFacade?.result?.submitSeq
-                        };
-
-                        onSubmitFormHandler(payload);
+                            submitId: submitSeq
+                        });
+                    } else {
+                        onSubmitFormHandler({
+                            type: 'FORM_SUBMITTED_ERROR'
+                        });
                     }
 
                 } else if (arg === 'cart-product' || arg === 'cart-checkout') {
@@ -46,11 +42,9 @@ const FormfacadeEmbed = ({
             },
         };
 
-        // @ts-ignore
         window.ffPrefillForm = () => {
-            console.log('prefillFormFn');
             try {
-                prefillFormFn();
+                return prefillFormFn();
             } catch (error) {
                 console.error(error);
             }
@@ -66,4 +60,9 @@ const FormfacadeEmbed = ({
     )
 }
 
-export default FormfacadeEmbed;
+const areEqual = (prevProps, nextProps) => {
+    return prevProps.formFacadeEmbedURL === nextProps.formFacadeEmbedURL;
+}
+
+
+export default React.memo(FormfacadeEmbed, areEqual);
