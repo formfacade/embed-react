fe2260d222beb3d5ca2d2956e21c4912bd8288db
HariKarthyk <harikarthyk1407@gmail.com>
2023-12-30 17:05:51 +0530

    Add .npmignore, .babelrc, rollup.config.js, and FormfacadeEmbed.jsx files

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import { babel } from '@rollup/plugin-babel';
+// import { uglify } from "rollup-plugin-uglify";
+// import uglify from "@lopatnov/rollup-plugin-uglify";
+// const packageJson = require('./package.json')
+
+export default [
+    {
+        input: './src/index.js', // Corrected the entry file to index.js
+        output: [
+            {
+                file: 'dist/index.js',
+                format: 'cjs',
+                sourcemap: false,
+            },
+            {
+                file: "dist/index.es.js",
+                format: "es",
+                exports: "named"
+            }
+        ],
+        external: [
+            'react',
+            'react-dom'
+        ],
+        plugins: [
+            peerDepsExternal(),
+            nodeResolve({
+                extensions: ['.js', '.jsx']
+            }),
+            commonjs(),
+            terser(),
+            postcss({
+                plugins: [],
+                minimize: true,
+                extract: 'style.css'
+            }),
+            babel({
+
+                // presets: ["@babel/preset-env", "@babel/preset-react"],
+                // extensions: ['.js', '.jsx']
+                configFile: './.babelrc',
+                babelHelpers: "bundled",
+                exclude: 'node_modules/**',
+            }),
+            // uglify()
+        ],
+    }
+]
