6c400afa5306ac35671895b5a97ce1ae64c3212d
HariKarthyk <harikarthyk1407@gmail.com>
2024-01-03 07:46:44 +0530

    Refactor FormfacadeEmbed component.

--- src/FormfacadeEmbed.jsx
+++ src/FormfacadeEmbed.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const onSubmitFormHandlerDefault = () => {
+const defaultSubmitHandler = () => {
     alert('Form submitted');
-}
+};
 
-const FormfacadeEmbed = ({
-    prefillFormFn = () => { return {}; },
-    formFacadeEmbedURL,
-    onSubmitFormHandler = onSubmitFormHandlerDefault
+const FormFacadeEmbed = ({
+    prefillForm = () => { return {}; },
+    formFacadeURL,
+    onSubmitForm = defaultSubmitHandler
 }) => {
-    React.useEffect(() => {
-
+    useEffect(() => {
         const script = document.createElement('script');
 
-        formFacadeEmbedURL = formFacadeEmbedURL + (formFacadeEmbedURL.indexOf('?') > -1 ? '&' : '?') + 'prefill=ffPrefillForm';
+        formFacadeURL = formFacadeURL + (formFacadeURL.indexOf('?') > -1 ? '&' : '?') + 'prefill=ffPrefillForm';
 
-        script.src = formFacadeEmbedURL;
+        script.src = formFacadeURL;
         script.async = true;
         script.defer = true;
         document.body.appendChild(script);
@@ -26,25 +25,24 @@ const FormfacadeEmbed = ({
                     const submitSeq = window.formFacade?.result && window.formFacade?.result?.submitSeq;
 
                     if (submitSeq) {
-                        onSubmitFormHandler({
+                        onSubmitForm({
                             type: 'FORM_SUBMITTED_SUCCESS',
                             submitId: submitSeq
                         });
                     } else {
-                        onSubmitFormHandler({
+                        onSubmitForm({
                             type: 'FORM_SUBMITTED_ERROR'
                         });
                     }
-
                 } else if (arg === 'cart-product' || arg === 'cart-checkout') {
-                    // TODO:
+                    // TODO: Handle cart related changes
                 }
             },
         };
 
         window.ffPrefillForm = () => {
             try {
-                return prefillFormFn();
+                return prefillForm();
             } catch (error) {
                 console.error(error);
             }
@@ -53,7 +51,7 @@ const FormfacadeEmbed = ({
         return () => {
             document.body.removeChild(script);
         }
-    }, [formFacadeEmbedURL]);
+    }, [formFacadeURL]);
 
     return (
         <div id='ff-compose'></div>
@@ -61,8 +59,7 @@ const FormfacadeEmbed = ({
 }
 
 const areEqual = (prevProps, nextProps) => {
-    return prevProps.formFacadeEmbedURL === nextProps.formFacadeEmbedURL;
+    return prevProps.formFacadeURL === nextProps.formFacadeURL;
 }
 
-
-export default React.memo(FormfacadeEmbed, areEqual);
+export default React.memo(FormFacadeEmbed, areEqual);
